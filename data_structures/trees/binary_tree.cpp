c27eda14a951aafcbd68ee82c68177a6c91488e7
AnuragBhonsle <anuragkbhonsle@gmail.com>
2025-09-06 09:11:54 +0530

    Added BFS (level-order) traversal alongside preorder, inorder, and postorder

--- data_structures/trees/binary_tree.cpp
+++ data_structures/trees/binary_tree.cpp
@@ -29,14 +29,38 @@ void postorder(Node* root) {
     cout << root->val << " ";
 }
 
+void bfs(Node* root) {
+    if(!root) return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()) {
+        Node* cur = q.front(); q.pop();
+        cout << cur->val << " ";
+        if(cur->left) q.push(cur->left);
+        if(cur->right) q.push(cur->right);
+    }
+}
+
 int main() {
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
 
+    cout << "Preorder: ";
     preorder(root);
+    cout << "\n";
+
+    cout << "Inorder: ";
     inorder(root);
+    cout << "\n";
+
+    cout << "Postorder: ";
     postorder(root);
+    cout << "\n";
+
+    cout << "BFS: ";
+    bfs(root);
+    cout << "\n";
 }
 
